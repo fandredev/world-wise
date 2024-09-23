b6080d161356708993bbc8ca98fb09752a2f2942
felipe <profissionalf.andre@gmail.com>
2024-09-23 18:21:55 -0300

    refactor: add login page and update navigation
    - Add Login component to the app
    - Update navigation to include login page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Product from './pages/product';
 import Pricing from './pages/pricing';
 import NotFound from './pages/not-found';
 import AppLayout from './pages/app-layout';
+import Login from './pages/login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
 
         {/* 404 */}
