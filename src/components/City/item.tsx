0f52e8a68248f13a30bb7478ab85b3ed06e96bb3
felipe <profissionalf.andre@gmail.com>
2024-09-25 00:41:11 -0300

    refactor: Update CityItem component to highlight the current city
    - Add useCities hook to access the current city from the context
    - Update Link className to highlight the current city item

--- src/components/City/item.tsx
+++ src/components/City/item.tsx
@@ -2,6 +2,7 @@ import { ICity } from './city-list';
 import styles from './city-item.module.css';
 import { formatDate } from '../../utils/format-date';
 import { Link } from 'react-router-dom';
+import { useCities } from '../../hooks/use-cities';
 
 interface CityItemProps {
   city: ICity;
@@ -16,9 +17,16 @@ export default function CityItem({ city }: CityItemProps) {
     position: { lat, lng },
   } = city;
 
+  const { currentCity } = useCities();
+
   return (
     <li>
-      <Link to={`${id}?lat=${lat}&lng=${lng}`} className={styles.cityItem}>
+      <Link
+        to={`${id}?lat=${lat}&lng=${lng}`}
+        className={`${styles.cityItem} ${
+          currentCity.id === id ? styles['cityItem--active'] : ''
+        }`}
+      >
         <span className={styles.emoji}>{emoji} </span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
